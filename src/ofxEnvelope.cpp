3713ed30791ac4ba2196cdded6cca8e5d9f99353
edapx <lastexxit@gmail.com>
2017-06-28 23:31:24 +0200

    gaussian dist on ray

--- src/ofxEnvelope.cpp
+++ src/ofxEnvelope.cpp
@@ -1,10 +1,12 @@
 #include "ofxEnvelope.h"
 
 static const ofxEnvelopeOptions defaultEnvelopeOptions = {
-    400, // curveHeight
-    1.0, // curveCutoff
+    400,   // curveHeight
+    1.0,   // curveCutoff
     0.002, // curvature
-    600 // nVertices
+    600,   // nVertices
+    0.3,   //deviationOnY (percent on the height)
+    0.3,    //deviationOnX (percent on the ray)
 };
 
 int curveHeight;
@@ -18,12 +20,12 @@ ofxEnvelope::ofxEnvelope(){
 
 ofxEnvelope::ofxEnvelope(ofxEnvelopeOptions opt){
     setup(opt);
-
-
 };
 
 ofxEnvelope::~ofxEnvelope(){
     //cleanup allocation
+    points.clear();
+    line.clear();
 }
 
 void ofxEnvelope::setup(ofxEnvelopeOptions opt){
@@ -42,21 +44,61 @@ void ofxEnvelope::setupPoints(){
     }
 
     for (int i =0; i< options.nVertices; i++) {
-        float randomY = ofRandom(options.curveHeight);
-        float xOnCurve = getVauleOnParabola(options.curvature, randomY, options.curveHeight/2, x_zero);
-        float radiusLength = abs(xOnCurve);
-        float randomTheta = 2.0f * 3.1415926f * ofRandom(1.0);
+        float randomY = getRandomY();
+        // do not add points that are not in the range of the interested section
+        if(randomY < options.curveHeight && randomY > 0) {
+            float xOnCurve = getVauleOnParabola(options.curvature, randomY, options.curveHeight/2, x_zero);
+            float radiusLength = abs(xOnCurve);
+            float radius = getRandomRadius(radiusLength);
+            float randomTheta = 2.0f * 3.1415926f * ofRandom(1.0);
 
-        float x = radiusLength * cosf(randomTheta);
-        float z = radiusLength * sinf(randomTheta);
+            float x = radius * cosf(randomTheta);
+            float z = radius * sinf(randomTheta);
 
-        glm::vec3 pos = glm::vec3(x,
-                                  randomY,
-                                  z);
+            glm::vec3 pos = glm::vec3(x,
+                                      randomY,
+                                      z);
 
-        points.push_back(pos);
+            points.push_back(pos);
+        };
     }
 };
+
+float ofxEnvelope::getRandomY(){
+    if(options.deviationOnY < 0.01){
+        return ofRandom(options.curveHeight);
+    } else {
+        float deviation = options.curveHeight * options.deviationOnY;
+        float rY = abs(ofRandomGaussian(options.curveHeight, deviation));
+        // we take just the bottom part of the curve
+        float bottomPart;
+        if (rY >= options.curveHeight) {
+            float diff = rY = options.curveHeight;
+            bottomPart = options.curveHeight - diff;
+        } else {
+            bottomPart = rY;
+        }
+        return bottomPart;
+    }
+}
+
+float ofxEnvelope::getRandomRadius(float _avgRay){
+    if(options.deviationOnX < 0.01){
+        return _avgRay;
+    } else {
+        float deviation = _avgRay * options.deviationOnX;
+        float radius = abs(ofRandomGaussian(_avgRay, deviation));
+        // we take just the bottom part of the curve
+        float bottomPart;
+        if (radius >= _avgRay) {
+            float diff = radius - _avgRay;
+            bottomPart = _avgRay - diff;
+        } else {
+            bottomPart = radius;
+        }
+        return bottomPart;
+    }
+}
 
 
 const vector<glm::vec3> ofxEnvelope::getPoints() {
@@ -90,6 +132,18 @@ const float ofxEnvelope::getPointZero(float a, float _height) const{
 const float ofxEnvelope::getVauleOnParabola(float a, float y, float y_0, float x_0) const{
     return a * ((y - y_0)*(y - y_0)) + x_0;
 
+}
+
+// https://github.com/openframeworks/openFrameworks/issues/224
+float ofxEnvelope::ofRandomGaussian(float mean, float stddev) {
+    std::normal_distribution<float>::param_type _param(mean, stddev);
+    distribution.param(_param);
+    return distribution(generator);
+}
+
+float ofRandomGaussian() {
+    //this assumes the parameters dont change just get another number from the distribution
+    //return distribution(generator);
 }
 
 void ofxEnvelope::clear(){
--- src/ofxEnvelope.h
+++ src/ofxEnvelope.h
@@ -1,12 +1,15 @@
 #pragma once
 
 #include "ofMain.h"
+#include <random>
 
 struct ofxEnvelopeOptions{
     int curveHeight;
     float curveCutoff;
     float curvature;
     int nVertices;
+    float deviationOnY;
+    float deviationOnX;
 };
 
 class ofxEnvelope{
@@ -21,6 +24,7 @@ public:
     void moveTo(glm::vec3 pos);
     void clear();
 
+
     ofxEnvelopeOptions options;
     const vector<glm::vec3> getPoints();
 private:
@@ -28,4 +32,12 @@ private:
     ofPolyline line;
     const float getPointZero(float a, float _height) const;
     const float getVauleOnParabola(float a, float x, float x_0, float y_0) const;
+
+    float getRandomY();
+    float getRandomRadius(float _avgRay);
+    float ofRandomGaussian(float mean, float stddev);
+    float ofRandomGaussian();
+
+    std::default_random_engine generator;
+    std::normal_distribution<float> distribution;
 };
